aa36f8285ac0cbcce579c5467ae1196b4ec6991b
immortal-p <immortalkil4oy@gmail.com>
2025-05-11 12:59:53 +0300

    adding a module for file parsing

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import parseTwoFiles from "../src/parse.js"
 
 const program = new Command();
 
@@ -9,6 +10,11 @@ program
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
     .option('-f, --format [type]', 'output format')
-    .argument('[type]', 'format')
-
+    .argument('<filePath1>', 'path to first file')
+    .argument('<filePath2>', 'path to second file')
+    .action((filePath1, filePath2) => {
+        const [data1, data2] = parseTwoFiles(filePath1, filePath2);
+        console.log(data1);
+        console.log(data2)
+    })
 program.parse()
