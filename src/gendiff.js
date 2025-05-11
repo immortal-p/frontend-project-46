2afe0f2cbc2998da7252f6631e5f61be00106844
immortal-p <immortalkil4oy@gmail.com>
2025-05-11 16:27:23 +0300

    feat: getDiff

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,8 +2,37 @@
 
 import { Command } from "commander";
 import parseTwoFiles from "../src/parse.js"
+import _ from 'lodash'
 
 const program = new Command();
+
+const genDiff = (data1, data2) => {
+    const allKeys = [...new Set([
+        ...Object.keys(data1),
+        ...Object.keys(data2)
+    ])].sort();
+
+    let result = '';
+
+    allKeys.forEach(key => {
+        const data1Val = data1[key];
+        const data2Val = data2[key];
+
+        const isEqual = JSON.stringify(data1Val) === JSON.stringify(data2Val);
+        if(isEqual){
+            result += `    ${key}: ${JSON.stringify(data1Val)}\n`
+        }else{
+            if(data1Val !== undefined){
+                result += `  - ${key}: ${JSON.stringify(data1Val)}\n`
+            }
+            if(data2Val !== undefined) {
+                result += `  + ${key}: ${JSON.stringify(data2Val)}\n`
+            }
+        }
+    });
+
+    return `{\n${result}}`;
+}
 
 program
     .name('gendiff')
@@ -14,7 +43,6 @@ program
     .argument('<filePath2>', 'path to second file')
     .action((filePath1, filePath2) => {
         const [data1, data2] = parseTwoFiles(filePath1, filePath2);
-        console.log(data1);
-        console.log(data2)
+        console.log(genDiff(data1, data2))
     })
 program.parse()
