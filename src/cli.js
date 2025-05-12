05ab5d47c37f00e7622cde48ececfc418f2f8500
immortal-p <immortalkil4oy@gmail.com>
2025-05-12 20:15:35 +0300

    refactor: extract genDiff to separate module

---
+++ src/cli.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import parseTwoFiles from "./parse.js"
+import genDiff from "./genDiff.js";
+
+const program = new Command();
+
+program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0')
+    .option('-f, --format [type]', 'output format')
+    .argument('<filePath1>', 'path to first file')
+    .argument('<filePath2>', 'path to second file')
+    .action((filePath1, filePath2) => {
+        const [data1, data2] = parseTwoFiles(filePath1, filePath2);
+        console.log(genDiff(data1, data2))
+    })
+
+program.parse()
