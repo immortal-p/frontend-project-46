2d2fca751c93610e33e23fba8563543a9be81f94
immortal-p <immortalkil4oy@gmail.com>
2025-05-12 21:43:03 +0300

    fix: test

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,9 @@
-// eslint.config.js
-import js from '@eslint/js';
-import globals from 'globals'; 
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
 
-export default [
-  js.configs.recommended, 
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node, 
-        ...globals.jest,
-      },
-    },
-    rules: {
-      'no-console': 'off', 
-    },
-  },
-];
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+]);
