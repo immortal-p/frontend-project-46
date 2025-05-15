0c067f2da917dff0fc0b76659fac7dc03a9ad7a3
immortal-p <immortalkil4oy@gmail.com>
2025-05-15 21:40:34 +0300

    refactor: code optimization

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,28 +1,16 @@
-/* eslint-disable no-undef */
 import genDiff from "../src/genDiff";
-import parseTwoFiles  from "../src/parsers";
+import { parsersFile, parseTwoFiles}  from "../src/parsers";
+import { test, expect} from '@jest/globals'
 
 test('genDiff', () => {
-    const [file1, file2] = parseTwoFiles('file1.json', 'file2.yaml');
-    const result = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`
+    const [file1, file2] = parseTwoFiles('file1.json', 'file2.json');
+    const result = parsersFile('result1.txt')
     expect(genDiff(file1, file2)).toEqual(result);
 })
 
 test('genDiff with different data types', () => {
   const [file3, file4] = parseTwoFiles('file3.json', 'file4.json');
-  const result = `{
-  - age: 39
-  + age: 39
-  + id: 02145
-    name: Reyes
-}`
+  const result = parsersFile('result2.txt')
     expect(genDiff(file3, file4)).toEqual(result)
 })
 
