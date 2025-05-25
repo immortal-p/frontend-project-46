30f06efca18e7005e0ea5ef33d9f8b9bec3d558e
immortal-p <immortalkil4oy@gmail.com>
2025-05-25 17:21:35 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -10,13 +10,14 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
+    .option('-f, --format <type>', 'output format', 'stylish')
     .argument('<filePath1>', 'path to first file')
     .argument('<filePath2>', 'path to second file')
-    .option('-f, --format <type>', 'output format', 'stylish')
     .action((filePath1, filePath2, options) => {
         const [data1, data2] = parsersTwoFiles(filePath1, filePath2);
-        const result = genDiff(data1, data2, options);
-        console.log(result)
+        const formatName = options.format;
+        const diff = genDiff(data1, data2, formatName);
+        console.log(diff);
     })
 
 program.parse()
